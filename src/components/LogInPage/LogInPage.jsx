68fccaa18378bbbc9706bc360b1120710383b394
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-01-02 00:04:41 +0300

    autorization almost done by TanStack Query

--- src/components/LogInPage/LogInPage.jsx
+++ src/components/LogInPage/LogInPage.jsx
@@ -1,7 +1,9 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+// eslint-disable-next-line no-unused-vars
 import { TOKEN_KEY_IN_LS } from '../../utils/constrans'
-import { api } from '../API/api'
+// import { api } from '../API/api'
 import { Modal } from '../Modal/Modal'
 import imgHeader from './imgLogInPage.jpg'
 import styles from './logInPageStyles.module.css'
@@ -14,6 +16,41 @@ import styles from './logInPageStyles.module.css'
 export function LogInPage() {
   const [isModalOpen, setIsModalOpen] = useState(false)
   const navigate = useNavigate()
+  const AUTORIZATION_QUERY = ['AUTORIZATION_QUERY']
+  const autorizationFn = async () => {
+    const asd = {
+      email: '4alexey.sitnikov@bk.ru',
+      group: 'sm8',
+      password: '12345',
+    }
+    const response = await fetch('https://api.react-learning.ru/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(asd),
+    })
+    const result = await response.json()
+    return result
+    // console.log(message)
+    // localStorage.setItem(TOKEN_KEY_IN_LS, JSON.stringify(response.token))
+    // })
+  }
+  // const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: autorizationFn,
+    onSuccess: () => {
+      // queryClient.invalidateQueries({ queryKey: AUTORIZATION_QUERY })
+    },
+  })
+
+  // eslint-disable-next-line no-unused-vars
+  const query = useQuery({
+    queryKey: AUTORIZATION_QUERY,
+    queryFn: autorizationFn,
+    refetchOnWindowFocus: false,
+    enabled: false,
+  })
 
   // const autorization = async () => {
   //   const response = await fetch('https://api.react-learning.ru/signin', {
@@ -43,16 +80,14 @@ export function LogInPage() {
     // }).catch(new Error('Ошибка авторизации'))
   }
 
-  const autorizationBtn = (e) => {
+  const autorizationBtn = async (e) => {
     e.preventDefault()
-    api.getAutorization()
-      .then((result) => {
-        localStorage.setItem(TOKEN_KEY_IN_LS, JSON.stringify(result.token))
-      }).then(() => {
-        navigate('/')
-      })
-      .catch(new Error('Ошибка авторизации'))
-    return null
+    const result = await mutation.mutateAsync()
+    console.log(result)
+    // console.log(mutation.error)
+    setTimeout(() => {
+      navigate('/')
+    }, 300)
   }
   // const closeModal = () => {
   //   // console.log({ e })
