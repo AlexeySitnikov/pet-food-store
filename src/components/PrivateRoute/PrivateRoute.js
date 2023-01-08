660ece2e79cb56a6aab8d4d3ff67a0e6f4b01b1c
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-01-08 22:24:36 +0300

    add tokenAC

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,14 @@
+import { useDispatch } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 // import { TOKEN_KEY_IN_LS } from '../../utils/constrans'
 import { api } from '../API/api'
+import { setTokenAC } from '../Redux/ActionCreators/tokenAC'
 
 export function PrivateRoute({ children }) {
   // const tokenFromLS = localStorage.getItem(TOKEN_KEY_IN_LS)
   const token = api.getToken()
+  const dispatch = useDispatch()
+  dispatch(setTokenAC(token))
 
   return token ? children : <Navigate to="login" />
 }
