51e8392b58980cd00dc790d3c64658a11d120ee3
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-01-18 12:23:23 +0300

    add discount, add changing cart button while pushing 'add to cart'

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -4,16 +4,20 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import styles from './cardProduct.module.css'
 import { addToCart } from '../Redux/Slices/productsSlice/productsSlice'
 
 export function ProductItem({ product }) {
+  const productsInCart = useSelector((cart) => cart.products)
   const dispatch = useDispatch()
   const addToCartHandler = (e) => {
     e.preventDefault()
     dispatch(addToCart(product))
   }
+
+  const discount = product.discount !== 0
+
   return (
     <div className={`${styles.product_item}`}>
       <div className={`${styles.product_img}`}>
@@ -22,11 +26,12 @@ export function ProductItem({ product }) {
       <div className={`${styles.product_list}`}>
         <h3>{product.name}</h3>
         <div className={`${styles.stars}`} />
-        <span className={`${styles.price}`}>{product.price}</span>
+        <span className={`${styles.discount}`}>{discount ? `${product.discount}%` : ''}</span>
+        <span className={discount ? `${styles.discount}` : `${styles.price}`}>{product.price - (product.price * product.discount) / 100}</span>
         <div className={`${styles.actions}`}>
           <div className={`${styles.add_to_cart}`}>
-            <button type="button" className={`${styles.cart_button}`} onClick={addToCartHandler}>
-              В корзину
+            <button type="button" className={(productsInCart.findIndex((item) => item.id === product._id) !== -1) ? `${styles.in_cart_button}` : `${styles.cart_button}`} onClick={addToCartHandler}>
+              {(productsInCart.findIndex((item) => item.id === product._id) !== -1) ? 'В корзине' : 'В корзину'}
             </button>
           </div>
           <div className={`${styles.add_to_links}`}>
