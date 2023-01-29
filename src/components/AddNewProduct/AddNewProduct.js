bc478966f6ee6b9a288268f4063b8b6dab0f9027
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-01-29 16:04:33 +0300

    add Formic component for add new product. No logic applied

--- src/components/AddNewProduct/AddNewProduct.js
+++ src/components/AddNewProduct/AddNewProduct.js
@@ -1,5 +1,27 @@
+import { Field, Form, Formik } from 'formik'
+
 export function AddNewProduct() {
+  const addNewProductClickHandler = () => {
+    console.log('click add new product')
+  }
+
   return (
-    <h1>Add new product page</h1>
+    <div className="">
+      <h1>Add new product page</h1>
+      <Formik
+        initialValues={{
+          ProductName: '', ProductDiscount: 0, ProductPrice: 0, ProductDiscription: '',
+        }}
+        onSubmit={addNewProductClickHandler}
+      >
+        <Form>
+          <Field name="ProductName" type="text" />
+          <Field name="ProductPrice" type="number" />
+          <Field name="ProductDiscription" type="text" />
+          <Field name="ProductDiscount" type="number" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
   )
 }
